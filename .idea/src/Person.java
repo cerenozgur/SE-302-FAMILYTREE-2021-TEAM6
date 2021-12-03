d856ea8f156360bf5b95137ba10bc58ace40e4e6
norgalathel <73163117+norgalathel@users.noreply.github.com>
2021-12-03 16:45:44 +0300

    Create Person.java

---
+++ .idea/src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String isim;
+    String soyisim;
+    Person anne;
+    Person baba;
+    Person partner;
+    Person cocuk1, cocuk2, cocuk3;
+    Person dede1;
+    Person dede2;
+    Person anneanne;
+    Person babaanne;
+}
