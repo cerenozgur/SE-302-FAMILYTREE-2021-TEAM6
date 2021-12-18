9bb3f92dcd170458598a97578bcf561ff629e653
norgalathel <73163117+norgalathel@users.noreply.github.com>
2021-12-18 14:41:21 +0300

    json write test edildi çalışıyor
    org.json.simple jarı eklendi
    org.json değiştirildi

--- .idea/src/Person.java
+++ .idea/src/Person.java
@@ -1,12 +1,60 @@
 public class Person {
     String isim;
     String soyisim;
+    int dogumYili;
+    String gender;
+    boolean hayattaMi;
     Person anne;
     Person baba;
     Person partner;
-    Person cocuk1, cocuk2, cocuk3;
-    Person dede1;
-    Person dede2;
-    Person anneanne;
-    Person babaanne;
+    int personId;
+   
+    public String getIsim() {
+        return this.isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return this.soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public int getDogumYili() {
+        return this.dogumYili;
+    }
+
+    public void setDogumYili(int dogumYili) {
+        this.dogumYili = dogumYili;
+    }
+
+    public Person getAnne() {
+        return this.anne;
+    }
+
+    public void setAnne(Person anne) {
+        this.anne = anne;
+    }
+
+    public Person getBaba() {
+        return this.baba;
+    }
+
+    public void setBaba(Person baba) {
+        this.baba = baba;
+    }
+
+    public Person getPartner() {
+        return this.partner;
+    }
+
+    public void setPartner(Person partner) {
+        this.partner = partner;
+    }
+
 }
--- .idea/src/Test.java
+++ .idea/src/Test.java
@@ -1,5 +1,23 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.simple.*;
 public class Test{
 public static void main(String[] args) {
     
+    JSONObject jsonObject = new JSONObject();
+    jsonObject.put("ID", "1");
+    jsonObject.put("First_Name", "Esra Melike");
+    jsonObject.put("Last_Name", "Ersoy");
+    jsonObject.put("Date_Of_Birth", "1998-01-09");
+    jsonObject.put("Place_Of_Birth", "Antalya");
+    jsonObject.put("Country", "Turkey");
+    try {
+       FileWriter file = new FileWriter("C:/try/mynewjsonfile.json");
+       file.write(jsonObject.toJSONString());
+       file.close();
+    } catch (IOException e) {
+       e.printStackTrace();
+    }
+    System.out.println("JSON file created: "+jsonObject);
 }
 }
