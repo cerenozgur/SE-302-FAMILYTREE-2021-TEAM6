f2672883e115537f3445418122693e003fc1f8a7
norgalathel <73163117+norgalathel@users.noreply.github.com>
2021-12-24 23:38:50 +0300

    teyze relationship denendi
    testleri yapılacak

---
+++ .idea/src/Person.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+
+public class Person {
+    String isim;
+    String soyisim;
+    int dogumYili;
+    String gender;
+    boolean hayattaMi;
+    Person anne;
+    Person baba;
+    Person partner;
+    ArrayList<Person> cocuklar = new ArrayList<Person>();
+    int personId;   
+
+    public void getTeyze(Person x){
+        ArrayList<Person> temp = new ArrayList<Person>();
+        if(x.cocuklar!=null){
+            for(int i=0; i<x.anne.anne.cocuklar.size(); i++){
+                if(x.anne.anne.cocuklar.get(i).gender=="Kadin"){
+                    temp.add(x.anne.anne.cocuklar.get(i));
+                }
+            }
+            System.out.println(x.isim + " kişisinin teyzeleri: ");
+            for(int i=0; i<temp.size(); i++){
+                System.out.println(temp.get(i).isim);
+            }
+        }
+        else{
+            System.out.println("Teyze bulunmuyor.");
+        }
+    }
+   
+    public String getIsim() {
+        return this.isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getSoyisim() {
+        return this.soyisim;
+    }
+
+    public void setSoyisim(String soyisim) {
+        this.soyisim = soyisim;
+    }
+
+    public int getDogumYili() {
+        return this.dogumYili;
+    }
+
+    public void setDogumYili(int dogumYili) {
+        this.dogumYili = dogumYili;
+    }
+
+    public Person getAnne() {
+        return this.anne;
+    }
+
+    public void setAnne(Person anne) {
+        this.anne = anne;
+    }
+
+    public Person getBaba() {
+        return this.baba;
+    }
+
+    public void setBaba(Person baba) {
+        this.baba = baba;
+    }
+
+    public Person getPartner() {
+        return this.partner;
+    }
+
+    public void setPartner(Person partner) {
+        this.partner = partner;
+    }
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public boolean isHayattaMi() {
+        return hayattaMi;
+    }
+
+    public void setHayattaMi(boolean hayattaMi) {
+        this.hayattaMi = hayattaMi;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+}
--- .idea/src/Test.java
+++ .idea/src/Test.java
@@ -1,5 +1,52 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Test{
 public static void main(String[] args) {
+
+    Person me = new Person();
+    Person anne = new Person();
+    me.setAnne(anne);
+    me.setIsim("Esra");
+
+    Person anneanne = new Person();
+    anne.setAnne(anneanne);
+
+    Person teyze = new Person();
+    teyze.setIsim("Ceren");
+    teyze.setGender("Kadin");
+    anneanne.cocuklar.add(teyze);
+
+    Person teyze1 = new Person();
+    teyze1.setIsim("Elif");
+    teyze1.setGender("Kadin");
+    anneanne.cocuklar.add(teyze1);
+
+    Person teyze2 = new Person();
+    teyze2.setIsim("Nergis");
+    teyze2.setGender("Kadin");
+    anneanne.cocuklar.add(teyze2);
+
+    me.getTeyze(me);
     
+
+    // try (Scanner scan1 = new Scanner(System.in)) {
+    //     System.out.println("write the name of the first person");
+    //     testPerson.isim = scan1.nextLine();
+    // }
+    // JSONObject jsonObject = new JSONObject();
+    // jsonObject.put("ID", "1");
+    // jsonObject.put("First_Name", testPerson.isim);
+    
+    // try {
+    //     FileWriter file = new FileWriter("C:/try/mynewjsonfile.json");
+    //     file.write(jsonObject.toJSONString());
+    //     file.close();
+    // } catch (IOException e) {
+    // e.printStackTrace();
+    // }
+    // System.out.println("JSON file created: "+jsonObject);
+
 }
 }
