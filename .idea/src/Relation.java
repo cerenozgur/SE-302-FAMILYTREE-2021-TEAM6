f5cbf674b7252b6c8fb4c9309ee1ec529f89ce0b
norgalathel <73163117+norgalathel@users.noreply.github.com>
2022-01-05 15:51:32 +0300

    second commit for relation class change

--- .idea/src/Relation.java
+++ .idea/src/Relation.java
@@ -19,4 +19,59 @@ public abstract class Relation{
         }
     }
 
+    public void getDayi(Person x){
+        ArrayList<Person> temp = new ArrayList<Person>();
+        if(x.cocuklar!=null){
+            for(int i=0; i<x.anne.anne.cocuklar.size(); i++){
+                if(x.anne.anne.cocuklar.get(i).gender=="Erkek"){ 
+                    temp.add(x.anne.anne.cocuklar.get(i));
+                }
+            }
+            System.out.println(x.isim + " kişisinin dayilari: ");
+            for(int i=0; i<temp.size(); i++){
+                System.out.println(temp.get(i).isim);
+            }
+        }
+        else{
+            System.out.println("Dayi bulunmuyor.");
+        }
+    }
+    public void getHala(Person x) {
+        ArrayList<Person> temp = new ArrayList<Person>();
+        if(x.cocuklar!=null){
+            for(int i=0; i<x.baba.anne.cocuklar.size(); i++){
+                if(x.baba.anne.cocuklar.get(i).gender=="Kadin"){ 
+                    temp.add(x.baba.anne.cocuklar.get(i));
+                }
+            }
+            System.out.println(x.isim + " kişisinin halalari: ");
+            for(int i=0; i<temp.size(); i++){
+                System.out.println(temp.get(i).isim);
+            }
+        }
+        else{
+            System.out.println("Hala bulunmuyor.");
+        }
+
+
+    }
+    
+    public void getAmca(Person x) {
+        ArrayList<Person> temp = new ArrayList<Person>();
+        if(x.cocuklar!=null){
+            for(int i=0; i<x.baba.anne.cocuklar.size(); i++){
+                if(x.baba.anne.cocuklar.get(i).gender=="Erkek"){ 
+                    temp.add(x.baba.anne.cocuklar.get(i));
+                }
+            }
+            System.out.println(x.isim + " kişisinin amcalari: ");
+            for(int i=0; i<temp.size(); i++){
+                System.out.println(temp.get(i).isim);
+            }
+        }
+        else{
+            System.out.println("Amca bulunmuyor.");
+        }
+    }
+
 }
