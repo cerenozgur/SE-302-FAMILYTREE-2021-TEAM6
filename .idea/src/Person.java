6889b1140463709ec916101be3c4a08cbd06e591
norgalathel <73163117+norgalathel@users.noreply.github.com>
2022-01-05 15:53:29 +0300

    third commit of relation

--- .idea/src/Person.java
+++ .idea/src/Person.java
@@ -11,67 +11,8 @@ public class Person extends Relation {
     Person partner;
     int personId;  
     ArrayList<Person> cocuklar = new ArrayList<Person>();
- 
-
-    
-    public void getDayi(Person x){
-        ArrayList<Person> temp = new ArrayList<Person>();
-        if(x.cocuklar!=null){
-            for(int i=0; i<x.anne.anne.cocuklar.size(); i++){
-                if(x.anne.anne.cocuklar.get(i).gender=="Erkek"){ 
-                    temp.add(x.anne.anne.cocuklar.get(i));
-                }
-            }
-            System.out.println(x.isim + " kişisinin dayilari: ");
-            for(int i=0; i<temp.size(); i++){
-                System.out.println(temp.get(i).isim);
-            }
-        }
-        else{
-            System.out.println("Dayi bulunmuyor.");
-        }
-    }
-    public void getHala(Person x) {
-        ArrayList<Person> temp = new ArrayList<Person>();
-        if(x.cocuklar!=null){
-            for(int i=0; i<x.baba.anne.cocuklar.size(); i++){
-                if(x.baba.anne.cocuklar.get(i).gender=="Kadin"){ 
-                    temp.add(x.baba.anne.cocuklar.get(i));
-                }
-            }
-            System.out.println(x.isim + " kişisinin halalari: ");
-            for(int i=0; i<temp.size(); i++){
-                System.out.println(temp.get(i).isim);
-            }
-        }
-        else{
-            System.out.println("Hala bulunmuyor.");
-        }
 
 
-    }
-    
-    public void getAmca(Person x) {
-        ArrayList<Person> temp = new ArrayList<Person>();
-        if(x.cocuklar!=null){
-            for(int i=0; i<x.baba.anne.cocuklar.size(); i++){
-                if(x.baba.anne.cocuklar.get(i).gender=="Erkek"){ 
-                    temp.add(x.baba.anne.cocuklar.get(i));
-                }
-            }
-            System.out.println(x.isim + " kişisinin amcalari: ");
-            for(int i=0; i<temp.size(); i++){
-                System.out.println(temp.get(i).isim);
-            }
-        }
-        else{
-            System.out.println("Amca bulunmuyor.");
-        }
-
-    }
-
-
-   
     public String getIsim() {
         return this.isim;
     }
